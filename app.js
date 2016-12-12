c0ec105ccdc1b9cd4daa886c658211cb75537f03
J1aDong <991383877@qq.com>
2016-12-12 17:32:19 +0800

    change text

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('Hello World!')
+    res.end('Hello I am from daocloud!')
 });
 
 server.listen(appPort, function() {
